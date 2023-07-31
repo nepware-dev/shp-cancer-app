3a7acf2323b2458477845fe5eaef8aae2d84380e
Dipendra Pathak <subaspathak62@gmail.com>
2023-07-31 14:29:12 +0545

    feat(splash-screen): splash screen in android and navigation

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: [
+      './jest-setup.js',
+      './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  transformIgnorePatterns: [
+      'node_modules/(?!(@react-native|react-native' +
+          '|@react-navigation' +
+          '|react-native-splash-screen' +
+          ')/)',
+  ],
+  testPathIgnorePatterns: ['src/vendor', 'node_modules'],
+  timers: 'fake',
 };
